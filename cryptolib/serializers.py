e27c83e9c1509ee3d928ffc9cb6927f1127eb739
vspaz <vladimir.edelman@gmail.com>
2021-10-31 16:58:50 +0100

    add 'Encoder' class

--- cryptolib/serializers.py
+++ cryptolib/serializers.py
@@ -59,3 +59,41 @@ def encrypt_text(unencrypted_text, cipher):
     except Exception as exc:
         logging.error(f'error encrypting text: {exc!r}')
         raise RuntimeError from exc
+
+
+class Encoder:
+    def __init__(self, private_key=None, public_key=None):
+        self._private_key = private_key
+        self._public_key = public_key
+
+    @staticmethod
+    def to_bytes(serializable, encoding='UTF-8'):
+        return serializable.encode(encoding=encoding)
+
+    @staticmethod
+    def from_bytes(deserializable, encoding='UTF-8'):
+        return deserializable.decode(encoding=encoding)
+
+    @staticmethod
+    def to_base_85(serializable):
+        return base64.a85encode(serializable)
+
+    @staticmethod
+    def from_base_85(deserializable):
+        return base64.a85decode(deserializable)
+
+    @staticmethod
+    def load_key(file_contents):
+        return RSA.importKey(file_contents)
+
+    @staticmethod
+    def _create_cipher(key):
+        return PKCS1_OAEP.new(key)
+
+    def encrypt_with_public_key(self, unencrypted_text):
+        cipher = self._create_cipher(key=self._public_key)
+        return cipher.encrypt(message=to_bytes(serializable=unencrypted_text))
+
+    def decrypt_with_private_key(self, encrypted_text):
+        cipher = self._create_cipher(key=self._private_key)
+        return cipher.decrypt(ct=encrypted_text)
