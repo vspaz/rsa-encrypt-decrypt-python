770593b1822dd4211ef9b56eb5d1c8517b752fca
vspaz <vladimir.edelman@gmail.com>
2021-11-01 20:55:10 +0100

    split 'Encoder' class

--- cryptolib/serializers.py
+++ cryptolib/serializers.py
@@ -4,13 +4,14 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
 
-class Encoder:
-    def __init__(self, private_key: str = None, public_key: str = None):
-        assert private_key or public_key, "private_key or/and public_key need to be given"
-        if private_key:
-            self._private_key = self.load_key(key=private_key.encode())
-        if public_key:
-            self._public_key = self.load_key(key=public_key.encode())
+class CryptoBase:
+    @staticmethod
+    def load_key(key):
+        return RSA.importKey(key)
+
+    @staticmethod
+    def _create_cipher(key):
+        return PKCS1_OAEP.new(key)
 
     @staticmethod
     def to_bytes(serializable: str, encoding='UTF-8') -> bytes:
@@ -20,26 +21,28 @@ class Encoder:
     def from_bytes(deserializable: bytes, encoding='UTF-8') -> str:
         return deserializable.decode(encoding=encoding)
 
-    @staticmethod
-    def to_base_85(serializable) -> bytes:
-        return base64.a85encode(serializable)
 
-    @staticmethod
-    def from_base_85(deserializable) -> bytes:
-        return base64.a85decode(deserializable)
-
-    @staticmethod
-    def load_key(key):
-        return RSA.importKey(key)
-
-    @staticmethod
-    def _create_cipher(key):
-        return PKCS1_OAEP.new(key)
+class Encoder(CryptoBase):
+    def __init__(self, public_key: str):
+        self._public_key = self.load_key(key=public_key.encode())
 
     def encrypt_with_public_key(self, unencrypted_text: str) -> bytes:
         cipher = self._create_cipher(key=self._public_key)
         return cipher.encrypt(message=unencrypted_text.encode())
 
+    @staticmethod
+    def to_base_85(serializable) -> bytes:
+        return base64.a85encode(serializable)
+
+
+class Decoder(CryptoBase):
+    def __init__(self, private_key: str):
+        self._private_key = self.load_key(key=private_key.encode())
+
     def decrypt_with_private_key(self, encrypted_text: bytes) -> bytes:
         cipher = self._create_cipher(key=self._private_key)
         return cipher.decrypt(ciphertext=encrypted_text)
+
+    @staticmethod
+    def from_base_85(deserializable) -> bytes:
+        return base64.a85decode(deserializable)
